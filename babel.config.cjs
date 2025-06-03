6ed5e30ecfef522eaba9d8a4bb5ce9ca21d6d7d1
Reinaldo <reinaldocrem125.rp@gmail.com>
2025-06-03 11:11:41 -0500

    Se corrigio  el nombre de la carpeta __test__

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,3 +1,10 @@
 module.exports = {
   presets: [['@babel/preset-env', {targets: {node: 'current'}}]]
 };
+
+/* ¿Por qué usar esto con Jest?
+Jest ejecuta tests en Node.js
+
+Node.js no soporta nativamente todos los features de ES6+
+
+Babel hace el "traducción" para que Jest pueda entender tu código */
